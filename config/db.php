2bec1734d047cc30da9b5353bbad4d7b8b5c9bd6
Nikhil Bava <105956640+NikhilBava1@users.noreply.github.com>
2025-08-18 17:27:54 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 // Database configuration for admin panel
 $host = "dpg-d2hfcrndiees73efetp0-a.oregon-postgres.render.com";
-$port = "5432"
+$port = "5432";
 $dbname = "staybyte1_83s5";
 $user = "staybyte1_83s5_user";
 $password = "nPpzEff0iEOM0eloVjoc8ksdoK7dJPPx";
