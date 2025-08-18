94070709b3b0e00bdd5bccb474f4ed4c1bf62b41
Nikhil Bava <105956640+NikhilBava1@users.noreply.github.com>
2025-08-18 17:42:37 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,11 +2,11 @@
 // Database configuration for admin panel
 $HOST = "dpg-d2hfcrndiees73efetp0-a.oregon-postgres.render.com";
 $port = "5432";
-$DATABASE_NAME = "dpg-d2hfcrndiees73efetp0-a.oregon-postgres.render.com"; = "staybyte1_83s5";
+$DATABASE_NAME = "staybyte1_83s5";
 $USER = "staybyte1_83s5_user";
 $USER = "nPpzEff0iEOM0eloVjoc8ksdoK7dJPPx";
 
-$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+$conn = pg_connect("host=$HOST port=$port dbname=$DATABASE_NAME user=$USER password=$PASS");
 
 if (!$conn) {
      die("Connection failed: " . pg_last_error());
