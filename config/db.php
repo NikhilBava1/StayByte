afc4d6380e3f6b284f3295890c9d6810aef31106
Nikhil Bava <105956640+NikhilBava1@users.noreply.github.com>
2025-08-18 17:12:30 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,13 +1,14 @@
 <?php
 // Database configuration for admin panel
-$db_host = "dpg-d2hfcrndiees73efetp0-a.oregon-postgres.render.com";
-$db_user = "staybyte1_83s5_user";
-$db_pass = "nPpzEff0iEOM0eloVjoc8ksdoK7dJPPx";
-$db_name = "staybyte1_83s5";
+$host = "dpg-d2hfcrndiees73efetp0-a.oregon-postgres.render.com";
+$port = "5432"
+$user = "staybyte1_83s5_user";
+$password = "nPpzEff0iEOM0eloVjoc8ksdoK7dJPPx";
+$dbname = "staybyte1_83s5";
 
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+     die("Connection failed: " . pg_last_error());
 }
 ?>
