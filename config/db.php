6af371868f5f2cef5d3701e3d239d08c47787d79
Nikhil Bava <105956640+NikhilBava1@users.noreply.github.com>
2025-08-18 17:45:43 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 // Database configuration for admin panel
 $HOST = "dpg-d2hfcrndiees73efetp0-a.oregon-postgres.render.com";
 $port = "5432";
-$DATABASE_NAME = "staybyte1_83s5";
+$DATABASE_NAME = "dpg-d2hfcrndiees73efetp0-a.oregon-postgres.render.com" = "staybyte1_83s5";
 $USER = "staybyte1_83s5_user";
 $USER = "nPpzEff0iEOM0eloVjoc8ksdoK7dJPPx";
 
@@ -12,3 +12,5 @@ if (!$conn) {
      die("Connection failed: " . pg_last_error());
 }
 ?>
+
+
