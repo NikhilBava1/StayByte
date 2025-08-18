c294663b21ca4f8385003860ddb06d457b7273db
Nikhil Bava <105956640+NikhilBava1@users.noreply.github.com>
2025-08-18 17:41:20 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,10 +1,10 @@
 <?php
 // Database configuration for admin panel
-$host = "dpg-d2hfcrndiees73efetp0-a.oregon-postgres.render.com";
+$HOST = "dpg-d2hfcrndiees73efetp0-a.oregon-postgres.render.com";
 $port = "5432";
-$dbname = "staybyte1_83s5";
-$user = "staybyte1_83s5_user";
-$password = "nPpzEff0iEOM0eloVjoc8ksdoK7dJPPx";
+$DATABASE_NAME = "dpg-d2hfcrndiees73efetp0-a.oregon-postgres.render.com"; = "staybyte1_83s5";
+$USER = "staybyte1_83s5_user";
+$USER = "nPpzEff0iEOM0eloVjoc8ksdoK7dJPPx";
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
